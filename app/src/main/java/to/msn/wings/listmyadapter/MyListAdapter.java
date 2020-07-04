b96098aee23390e2b24842f15565dbd5443d101b
inouetakefumi <janja1120@yahoo.co.jp>
2020-07-04 15:38:46 +0900

    202007041530

--- app/src/main/java/to/msn/wings/listmyadapter/MyListAdapter.java
+++ app/src/main/java/to/msn/wings/listmyadapter/MyListAdapter.java
@@ -45,14 +45,25 @@ public class MyListAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
         Activity activity = (Activity) context;
         Schedule schedule = (Schedule) getItem(position);
         if (convertView == null) {
             convertView = activity.getLayoutInflater()
                     .inflate(R.layout.list_item, null);
-        }
-        ((TextView) convertView.findViewById(R.id.date)).setText(schedule.getDate().toString());
-        ((TextView) convertView.findViewById(R.id.work)).setText(schedule.getWork());
+
+            viewHolder = new ViewHolder();
+            viewHolder.date = (TextView) convertView.findViewById(R.id.date);
+            viewHolder.work = (TextView) convertView.findViewById(R.id.work);
+            convertView.setTag(viewHolder);
+        } else {
+        viewHolder = (ViewHolder) convertView.getTag();
+    }
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+        String formatDate = sdf.format(schedule.getDate());
+        viewHolder.date.setText(formatDate);
+        viewHolder.work.setText(schedule.getWork());
+
         return convertView;
     }
 }

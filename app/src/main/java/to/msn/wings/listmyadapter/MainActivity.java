8d50bcb680cb0f163b481caa719a52f53057c5a8
inouetakefumi <janja1120@yahoo.co.jp>
2020-08-08 21:11:22 +0900

    202008082100

--- app/src/main/java/to/msn/wings/listmyadapter/MainActivity.java
+++ app/src/main/java/to/msn/wings/listmyadapter/MainActivity.java
@@ -20,7 +20,6 @@ import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Realm mRealm;
     private Realm pRealm;
     private ListView list;
 
@@ -32,97 +31,33 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setActionBar(toolbar);
 
-        RealmConfiguration config = new RealmConfiguration.Builder()
-                .name("schedule.realm")
-                .schemaVersion(1)
-                .build();
-
-        mRealm.deleteRealm(config);
-
-        mRealm = Realm.getInstance(config);
-
         RealmConfiguration profile = new RealmConfiguration.Builder()
                 .name("profile.realm")
-                .schemaVersion(2)
+                .schemaVersion(3)
                 .build();
 
         pRealm.deleteRealm(profile);
 
         pRealm = Realm.getInstance(profile);
 
-        //データを100日初期登録
-        try {
-            for(int i = 0;i<100;i++){
-                mRealm.executeTransaction(new RealmInitTransaction(i) {
-                });
-            }
-        }
-        catch (IllegalArgumentException e) {
-            // 不正な日付の場合の処理
-        }
+        //profileを初期登録
 
         pRealm.executeTransaction(new RealmProfileTransaction() {
         });
 
-        Calendar today = Calendar.getInstance();
-        today.set(Calendar.HOUR_OF_DAY, 0);
-        today.set(Calendar.MINUTE, 0);
-        today.set(Calendar.SECOND, 0);
-        today.set(Calendar.MILLISECOND, 0);
-        // today は　今日の日付の0時0分0.000秒
-        Date todayDate = today.getTime();
-        RealmResults<Schedule> result = mRealm.where(Schedule.class)
-                .greaterThanOrEqualTo("date", todayDate)
+        //adapterにprofileDBを登録
+        RealmResults<Profile> profileresult = pRealm.where(Profile.class)
                 .findAll();
-        ArrayList<Schedule> data = new ArrayList<>();
+        ArrayList<Profile> data = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
-            data.add(result.get(i));
+            data.add(profileresult.get(i));
         }
-        MyListAdapter adapter = new MyListAdapter(this, data, R.layout.list_item);
+        ProfileAdapter adapter = new ProfileAdapter(this, data, R.layout.list_name);
         ListView list = findViewById(R.id.list);
         list.setAdapter(adapter);
-
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                MyListAdapter adapter = (MyListAdapter) parent.getAdapter();
-                Schedule schedule = (Schedule) adapter.getItem(position);
-                Intent intent = new Intent(MainActivity.this, ScheduleActivity.class);
-                intent.putExtra("ID",schedule.getId());
-                startActivity(intent);
-            }
-        });
     }
 
-    class RealmInitTransaction implements Realm.Transaction {
-        int i =0;
-        RealmInitTransaction(int i){
-            this.i = i;
-        }
-        @Override
-        public void execute(Realm realm) {
-            Number max = realm.where(Schedule.class).max("id");
-            long newId = 0;
-            if(max != null) { // nullチェック
-                newId = max.longValue() + 1;
-            }
-            Schedule schedule
-                    = realm.createObject(Schedule.class, newId);
 
-            Calendar c = Calendar.getInstance();
-
-            //当日日付を取得
-            int year = c.get(Calendar.YEAR);
-            int month = c.get(Calendar.MONTH);
-            int day= c.get(Calendar.DATE);
-            //日付を
-            c.set(year, month , day+i);
-            Date d = c.getTime();
-            schedule.date = d;
-            schedule.name = "井上";
-
-        }
-    }
     class RealmProfileTransaction implements Realm.Transaction {
         public void execute(Realm realm) {
             Number max = realm.where(Profile.class).max("id");
@@ -133,34 +68,16 @@ public class MainActivity extends AppCompatActivity {
             Profile profile
                     = realm.createObject(Profile.class, newId);
 
-
             profile.name = "井上";
+            profile.kana = "イノウエ";
             profile.phonenumber= "090-xxxx-xxxx";
+            profile.mail = "xxx@xxx";
+            profile.part = "第一システム担当";
 
         }
     }
 
-    protected void onResume() {
-        super.onResume();
 
-        Calendar today = Calendar.getInstance();
-        today.set(Calendar.HOUR_OF_DAY, 0);
-        today.set(Calendar.MINUTE, 0);
-        today.set(Calendar.SECOND, 0);
-        today.set(Calendar.MILLISECOND, 0);
-        // today は　今日の日付の0時0分0.000秒
-        Date todayDate = today.getTime();
-        RealmResults<Schedule> result = mRealm.where(Schedule.class)
-                .greaterThanOrEqualTo("date", todayDate)
-                .findAll();
-        ArrayList<Schedule> data = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            data.add(result.get(i));
-        }
-        MyListAdapter adapter = new MyListAdapter(this, data, R.layout.list_item);
-        ListView list = findViewById(R.id.list);
-        list.setAdapter(adapter);
-    }
 
 
 }
